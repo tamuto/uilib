fdad07f6a51ea6037add70b3804415a0fdaa58e1
tamuto <mutomob@gmail.com>
2023-08-28 06:15:22 +0000

    ダウンロード用コンポーネント削除（保守していないため）

--- components/form/FormBar.js
+++
@@ -1,8 +0,0 @@
-import styled from '@emotion/styled'
-
-const FormBar = styled.form`
-  display: flex;
-  gap: 20px 16px;
-`
-
-export default FormBar
--- components/form/Spacer.js
+++
@@ -1,7 +0,0 @@
-import styled from '@emotion/styled'
-
-const Spacer = styled.div`
-  flex-grow: 1;
-`
-
-export default Spacer
